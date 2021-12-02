30facc9b0c642d74838b7748e6c572d4d78d67f1
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-12-02 10:24:26 -0800

    writing functions

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,11 +1,41 @@
 import React from 'react';
 
-export default function Editor({ setHead, setMiddle, setBottom, setCatchphrase }) {
+export default function Editor({
+  catchphrase,
+  setHeadCount,
+  setHead,
+  setMiddle,
+  setBottom,
+  setCatchphrase,
+  setCatchphraseList,
+  setCatchphraseCount,
+  // handleHeadCount,
+  handleMiddleCount,
+  handleBottomCount,
+  // handleClick,
+  // handleCatchphraseCount,
+}) {
+  const handleClick = () => {
+    setCatchphraseList((prevState) => [...prevState, catchphrase]);
+
+    setCatchphrase('');
+  };
+
+  const handleCatchphraseCount = (e) => {
+    setCatchphrase(e.target.value);
+    setCatchphraseCount((prevState) => prevState + 1);
+  };
+
+  const handleHeadCount = (e) => {
+    setHead(e.target.value);
+    setHeadCount((prevState) => prevState + 1);
+  };
+
   return (
     <>
       <div className="editor">
         <label>Head</label>
-        <select onChange={(e) => setHead(e.target.value)}>
+        <select value={setHead} onChange={handleHeadCount}>
           <option value="Bird">Bird</option>
           <option value="Duck">Duck</option>
           <option value="Dog">Dog</option>
@@ -15,7 +45,7 @@ export default function Editor({ setHead, setMiddle, setBottom, setCatchphrase }
 
       <div className="editor">
         <label>Middle</label>
-        <select onChange={(e) => setMiddle(e.target.value)}>
+        <select value={setMiddle} onChange={handleMiddleCount}>
           <option value="Blue">Blue</option>
           <option value="Fancy">Fancy</option>
           <option value="Pink">Pink</option>
@@ -25,7 +55,7 @@ export default function Editor({ setHead, setMiddle, setBottom, setCatchphrase }
 
       <div className="editor">
         <label>Bottom</label>
-        <select onChange={(e) => setBottom(e.target.value)}>
+        <select value={setBottom} onChange={handleBottomCount}>
           <option value="leg">Single Leg</option>
           <option value="white">White Pants</option>
           <option value="blue">Blue Jeans</option>
@@ -36,7 +66,7 @@ export default function Editor({ setHead, setMiddle, setBottom, setCatchphrase }
       <div className="editor">
         <label>Catch Phrase!</label>
         <input type="text" onChange={(e) => setCatchphrase(e.target.value)} />
-        <button>Add!</button>
+        <button onChange={handleClick}>Add!</button>
       </div>
     </>
   );
