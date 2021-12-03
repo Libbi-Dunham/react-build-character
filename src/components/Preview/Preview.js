8e4dcac4259fadd56ad681b24e9669bfdd45471c
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-12-03 13:03:30 -0800

    added back in a preview

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import './Preview.css';
 
-export default function Preview({ head, middle, bottom }) {
+export default function Preview({ head, middle, bottom, catchphrase }) {
   return (
     <>
       <div className="clothes">
         <img height="75" alt={head} src={`${process.env.PUBLIC_URL}/${head}-head.png`} />
         <img height="75" alt={middle} src={`${process.env.PUBLIC_URL}/${middle}-middle.png`} />
         <img height="75" alt={bottom} src={`${process.env.PUBLIC_URL}/${bottom}-pants.png`} />
+        <p> {catchphrase} </p>
       </div>
     </>
   );
